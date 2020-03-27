860f9fcfe9bc9fb6c4ab7ea4a7dd17ef440b9fdb
Philip <philsavenok@gmail.com>
2020-03-27 16:56:50 -0400

    user post

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,9 +1,57 @@
 const express = require('express')
+const { check, validationResult } = require('express-validator')
+const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs')
+const User = require('../../models/user')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('User route')
+router.post('/', [
+    check('name', 'name is required').not().isEmpty(),
+    check('email', 'please include a valid email').isEmail(),
+    check('password', 'please enter a password with more than 6 characters').isLength({ min: 6 })
+], async (req, res) => {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { name, email, password } = req.body
+
+    try {
+        //Check if user exists
+        let user = await User.findOne({ email })
+
+        if (user) {
+            return res.status(400).json({ errors: [{ msg: "User already exists" }] })
+        }
+
+        //Creating avatar
+        const avatar = gravatar.url(email, {
+            s: '200',  //size
+            r: 'pg',   //raiting
+            d: 'mm'    //default image
+        })
+
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        })
+        
+        const salt = await bcrypt.genSalt(10) //10 is default for security lvl
+        user.password = await bcrypt.hash(password, salt)
+
+        await user.save()
+
+        res.send('User registered')
+    } catch (e) {
+        console.error(e.message)
+        res.status(500).send('Server error')
+    }
+    
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const connectDB = require('./config/db')
 
 const app = express()
 const PORT = process.env.PORT || 3000
-connectDB()
+
+connectDB()  //Conntect mongoDB
+
+app.use(express.json({ extended: false }))  //Init middleware like req.body
 
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/posts', require('./routes/api/posts'))
