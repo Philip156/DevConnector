e01e9db9e973af9079be4c4131c397c953d7df60
Philip <philsavenok@gmail.com>
2020-03-27 18:33:32 -0400

    passing user token to auth route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const auth = require('../../middleware/auth')
+const User = require('../../models/user')
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('Auth route')
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password')
+        res.json(user)
+    } catch (e) {
+        console.log(e.message)
+        res.status(500).send('Server error')
+    }
 })
 
 module.exports = router
