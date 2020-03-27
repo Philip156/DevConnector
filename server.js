b22cdcaef30da5744e59cc433e95854cdde36fa8
Philip <philsavenok@gmail.com>
2020-03-27 09:40:16 -0400

    adding routes

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express()
 const PORT = process.env.PORT || 3000
 connectDB()
 
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/user', require('./routes/api/user'))
+
 app.get('/', (req, res) => {
     res.send('Welcome')
 })
